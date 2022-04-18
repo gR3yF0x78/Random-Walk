8ef0d69af2d13f2dde253979d599b7aa6dbe5b65
gR3yF0x78 <simonfoxparker@protonmail.com>
2022-04-18 21:59:30 +0100

    Random RGB color

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-from turtle import Turtle, Screen
+import turtle as t
+from turtle import Screen
 import random
-from random import randint
 
-t = Turtle()
-t.shape("arrow")
-t.color("green")
-t.pensize(12)
-t.speed("fast")
+tim = t.Turtle()
+t.colormode(255)
+tim.shape("arrow")
+tim.color("green")
+tim.pensize(12)
+tim.speed("fast")
 
 screen = Screen()
+screen.colormode(0)
 screen.bgcolor("#0a0a0a")
 
 # Generate random custom colours --start
-color = []
-n = 10
-for i in range(n):
-    color.append('#%06X' % randint(0, 0xFFFFFF))
+# color = []
+# n = 10
+# for i in range(n):
+#     color.append('#%06X' % randint(0, 0xFFFFFF))
 # Generate random custom colours --end
+
+# Generate random RGB --start
+def random_rgb_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+# Generate random RGB --end
 
 direction = [0, 90, 180, 270]
 
 for number in range(200):
-    t.color(random.choice(color))
-    t.width(4)
-    t.forward(30)
-    t.setheading(random.choice(direction))
+    tim.color(random_rgb_color())
+    tim.width(4)
+    tim.forward(30)
+    tim.setheading(random.choice(direction))
 
 screen.exitonclick()
